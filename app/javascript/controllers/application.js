d358828eaff405d7dd586661b5d85bf8e75b6992
irum <irum51320@gmail.com>
2024-11-05 13:42:11 +0500

    initial commit

---
+++ app/javascript/controllers/application.js
@@ -0,0 +1,13 @@
+import "@hotwired/turbo-rails"
+import { Application } from "@hotwired/stimulus"
+import HelloController from "./hello_controller"
+
+const application = Application.start()
+application.register("hello", HelloController)
+
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus   = application
+
+
+export { application }
